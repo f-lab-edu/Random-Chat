19fcb385684af38e5122aab3cdf8f4e5f027a52d
Doncham <doncham98@gmail.com>
2024-09-15 16:57:41 +0900

    스프링 프로젝트 초기화

---
+++ RanChat/src/main/java/com/example/ranchat/RanChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.ranchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RanChatApplication.class, args);
+    }
+
+}
