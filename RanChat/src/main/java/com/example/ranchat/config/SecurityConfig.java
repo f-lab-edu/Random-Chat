f40fcb625d481ddcaa032b74e2f4e2897da67f86
Kimsangwon <doncham98@gmail.com>
2024-11-11 22:38:35 +0900

    feat: redis test를 위한 코드 작성

--- RanChat/src/main/java/com/example/ranchat/config/SecurityConfig.java
+++ RanChat/src/main/java/com/example/ranchat/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/api/user/login", "/", "/api/user/join","/api/user/test").permitAll()
+                        .requestMatchers("/api/user/login", "/", "/api/user/join","/api/user/test","/api/redis/**").permitAll()
                         .anyRequest().authenticated());
         http
                 .addFilterBefore(new JWTFilter(jwtParser), LoginFilter.class);
